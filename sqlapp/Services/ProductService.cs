1da3bc0fbe7b4b00fdb3d3aec728063d7b7be056
mnishanth02 <mnishanth02@mnishanth02>
2022-08-05 16:50:32 +0000

    Add project files.

---
+++ sqlapp/Services/ProductService.cs
@@ -0,0 +1,59 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+    public class ProductService
+    {
+
+        private static string db_source = "appservernm.database.windows.net";
+        private static string db_user = "mnishanth02";
+        private static string db_password = "Mn_pass@2022";
+        private static string db_database = "demodatabase01";
+
+
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Product> _product_list = new List<Product>();
+
+            string statement = "SELECT ProductID, ProductName, Quantity from Products";
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Product product = new Product()
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+
+                    };
+                    _product_list.Add(product);
+                }
+             
+            }
+            conn.Close();
+            return _product_list;
+        }
+        
+
+    }
+}
